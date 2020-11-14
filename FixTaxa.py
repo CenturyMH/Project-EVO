ddcbb41fdf73c667dcf2c1ebdb97909ab2889dc2
unknown <martinhaghverdian@gmail.com>
2020-11-13 19:48:25 -0800

    playing with regex

--- FixTaxa.py
+++ FixTaxa.py
@@ -9,10 +9,10 @@ def taxa_id(taxonID):
 
         for line in f:
             if taxonID in line:
-                firstWord = re.search("[^\s]+", line)
+                firstWord = re.search(r"[^\s]+", line)
                 if firstWord.group(0) == taxonID:
                     id_list.append(line)
-        
+
 def vernac_id(taxonID):
     with open(r"C:\Users\Martin\Desktop\Taxonomy\vernacular.txt", mode = "r", encoding="utf-8") as f:
         for line in f:
@@ -29,6 +29,5 @@ taxa_id("35489682")
 for i in id_list:
     print(i, "\n")
 
-# File reading doesn't work anymore for some reason
 # Next goal: Make every taxonID unique in the file to the species (Get rid of subspecies)
 # to run taxa_id here, taxa.txt cannot be open
--- Marathon.py
+++ Marathon.py
@@ -1,6 +1,16 @@
+import re
+
 def readwrite():
     with open(r"C:\Users\Martin\Desktop\python_test\textfiletest.txt", mode="r") as f:
         for line in f:
+            # firstletter = "d"
+            # if firstletter in line:
+            # numtest = re.search("\d", line)
+            # if numtest.group()
             print(line)
 
-readwrite()
+# readwrite()
+test = "abc123"
+numtest = re.search(r"\d+", test)
+
+print(numtest.group(0))
