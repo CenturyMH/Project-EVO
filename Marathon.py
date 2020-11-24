d429502ab53f20b50231f1ad78e1366d6aee1520
unknown <martinhaghverdian@gmail.com>
2020-11-23 17:29:42 -0800

    finding new ways to iterate through files

--- Marathon.py
+++ Marathon.py
@@ -1,22 +1,21 @@
 import re
 
-def rewrite(id):
+lines_to_write = []
+def rewrite():
     with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r") as f:
-        for line in f:
-            print(line, end="")
+        lines = f.read().splitlines()
+        # print(lines)
+        print(f.readlines()
+        for iter1 in lines:
+            first1 = re.match(r"[^\s]+", iter1).group()
+            for iter2 in lines:
+                first2 = re.match(r"[^\s]+", iter2).group()
+                if first1 in iter2 and first1 != first2:
+                    lines_to_write.append(iter2)
 
-        print("\n\n")
-        f.seek(0)
-        for line in f:
-            first_item = re.match(r"[^\s]+", line).group()
-            if id in line:
-                if first_item == id:
-                    print(line, end="")
-                else:
-                    pass
+rewrite()
 
-rewrite("123")
-
+# syntax error in for loop i dont understand
 # 3865179 lines were counted by looping through taxa.txt, took about 8-9 minutes
 # https://stackoverflow.com/questions/11469228/replace-and-overwrite-instead-of-appending/11469328#11469328
 # try writing to new file instead of rewriting existing file, open first one in read and second in write
