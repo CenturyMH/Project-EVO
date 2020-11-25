f793532a3edb41fc5f33cdc0b3a7d2cdfa4a1596
unknown <martinhaghverdian@gmail.com>
2020-11-25 00:31:47 -0800

    trying more things to iterate taxa.txt

--- Marathon.py
+++ Marathon.py
@@ -5,17 +5,18 @@ def rewrite():
     with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r") as f:
         lines = f.read().splitlines()
         # print(lines)
-        print(f.readlines()
+        print(f.readlines())
         for iter1 in lines:
-            first1 = re.match(r"[^\s]+", iter1).group()
+            taxonID = re.match(r"[^\s]+", iter1).group()
             for iter2 in lines:
-                first2 = re.match(r"[^\s]+", iter2).group()
-                if first1 in iter2 and first1 != first2:
+                if taxonID in iter2 and taxonID != re.match(r"[^\s]+", iter2).group():
                     lines_to_write.append(iter2)
 
 rewrite()
+print(lines_to_write)
 
-# syntax error in for loop i dont understand
+# Try: in first loop, store position of line of taxonID and use that to exclude lines with
+# taxonID that are not in the same position?
 # 3865179 lines were counted by looping through taxa.txt, took about 8-9 minutes
 # https://stackoverflow.com/questions/11469228/replace-and-overwrite-instead-of-appending/11469328#11469328
 # try writing to new file instead of rewriting existing file, open first one in read and second in write
