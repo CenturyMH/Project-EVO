e94a2f0d6f413a72581ac7e7a9e8297edb29b456
unknown <martinhaghverdian@gmail.com>
2020-11-30 18:56:20 -0800

    learning even more ways to modify this algorithm wow!~

--- Marathon.py
+++ Marathon.py
@@ -1,19 +1,27 @@
 import re
 
 lines_to_write = []
-def rewrite():
-    with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r") as f:
-        for position1, iter1 in enumerate(f):
-            taxonID1 = re.match(r"[^\s]+", iter1).group()
-            for position2, iter2 in enumerate(f):
-                taxonID2 = re.match(r"[^\s]+", iter2).group()
-                if taxonID1 in iter2:
-                    if position1 == position2:
-                        lines_to_write.append(iter1)
+# def rewrite():
+with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r") as f:
+    lines = f.readlines()
+        # for position1, iter1 in enumerate(f):
+        #     taxonID1 = re.match(r"[^\s]+", iter1).group()
+        #     for position2, iter2 in enumerate(f):
+        #         taxonID2 = re.match(r"[^\s]+", iter2).group()
+        #         if taxonID1 in iter2:
+        #             if position1 == position2:
+        #                 lines_to_write.append(iter1)
                     # else:
 #                         print(position1, position2)
+for line in lines:
+    print(line)
 
-rewrite()
+print("\n\n")
+for line in lines:
+    if "456" == re.match(r"[^\s]+", line).group():
+        print(line)
+
+# rewrite()
 # print(lines_to_write)
 
 # TRY: seek() counts every character in file, so use length of line to find position
