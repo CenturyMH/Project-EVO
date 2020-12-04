05532b7e57f203235023bd274d2bba9e5d58a731
unknown <martinhaghverdian@gmail.com>
2020-12-03 23:17:24 -0800

    First working version of algorithm

--- Marathon.py
+++ Marathon.py
@@ -1,23 +1,18 @@
 import re
 
 id_list = []
-# def rewrite():
 with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r") as file1:
-    lines = file1.readlines()
-    for line in lines:
-        id_list.append(re.match(r"[^\s]+", line).group())
+    lines_list = [line[:-1].split(" ") for line in file1]
+    for line in lines_list:
+        id_list.append(line[0])
 
 
 with open(r"C:\Users\Martin\Desktop\python_test\testing2.txt", mode="w+") as file2:
-    new_id_list = []
-    for line in lines:
-        if re.match(r"[^\s]+", line).group() not in file2:
-            pass
+    for i, id in enumerate(id_list):
+        if any([id in line and id != line[0] for line in lines_list]):
+            file2.write(str(lines_list[i]))
 
-# rewrite()
-# print(lines_to_write)
-
-# https://stackoverflow.com/questions/4710067/how-to-delete-a-specific-line-in-a-file
+# logic in algorithm works, need to modify it (line doesn't get written to file in preferred format)
 # 3865179 lines were counted by looping through taxa.txt, took about 8-9 minutes
 # https://stackoverflow.com/questions/11469228/replace-and-overwrite-instead-of-appending/11469328#11469328
 # try writing to new file instead of rewriting existing file, open first one in read and second in write
