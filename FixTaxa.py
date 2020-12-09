596caf32cbb52eb8372ecd1f1c59c4d8b413db86
unknown <martinhaghverdian@gmail.com>
2020-12-08 21:52:41 -0800

    burnout???

--- FixTaxa.py
+++ FixTaxa.py
@@ -1,22 +1,7 @@
 import re
 
 
-def taxa_id():
-    id_list = []
-    with open(r"C:\Users\Martin\Desktop\Taxonomy\taxa.txt", mode="r", encoding="utf-8") as file1:
-        lines_list = [line[:-1].split(" ") for line in file1]
-        for line in lines_list:
-            id_list.append(line[0])
-
-    with open(r"C:\Users\Martin\Desktop\Taxonomy\new_taxa.txt", mode="w+", encoding="utf-8") as file2:
-        for i, id in enumerate(id_list):
-            if any([id in line and id != line[0] for line in lines_list]):
-                file2.write(" ".join(lines_list[i]) + "\n")
-                print(i)
-
 
 # Killer Whale: 35489682
-taxa_id()
-
 # Next goal: Make every taxonID unique in the file to the species (Get rid of subspecies)
 # 3865179 lines were counted by looping through taxa.txt, took about 8-9 minutes
--- Marathon.py
+++ Marathon.py
@@ -2,9 +2,8 @@ import re
 
 
 with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r", encoding="utf-8") as file1:
-    pass
     with open(r"C:\Users\Martin\Desktop\python_test\testing2.txt", mode="w+", encoding="utf-8") as file2:
-        pass    
-
+        for line in file1:
+            id = re.search(r"([^\s]+)", line).group()
 
 # Goal: rewrite algorithm without using a list
