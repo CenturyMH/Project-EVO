fe87db0523e761b5e918183609b615c73a0c584a
unknown <martinhaghverdian@gmail.com>
2020-12-09 22:10:58 -0800

    trying

--- FixTaxa.py
+++ FixTaxa.py
@@ -1,6 +1,19 @@
 import re
 
+id_list = []
+with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r", encoding="utf-8") as file1:
+    lines_list = [line[:-1].split(" ") for line in file1]
+    for line in lines_list:
+        id_list.append(line[0])
 
+
+with open(r"C:\Users\Martin\Desktop\python_test\testing2.txt", mode="w+", encoding="utf-8") as file2:
+    for i, id in enumerate(id_list):
+        if any([id in line and id != line[0] for line in lines_list]):
+            file2.write(" ".join(lines_list[i]) + "\n")
+            print(i)
+
+print(id_list)
 
 # Killer Whale: 35489682
 # Next goal: Make every taxonID unique in the file to the species (Get rid of subspecies)
