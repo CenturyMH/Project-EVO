1281a50afe8d65716fce6e41a909715d3d7f717e
unknown <martinhaghverdian@gmail.com>
2020-12-17 21:48:43 -0800

    algorithm modified, still doesn't work on taxa.txt

--- FixTaxa.py
+++ FixTaxa.py
@@ -1,15 +1,17 @@
 import re
 
 id_list = []
-with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r", encoding="utf-8") as file1:
+with open(r"C:\Users\Martin\Desktop\Taxonomy\taxa.txt", mode="r", encoding="utf-8") as file1:
     lines_list = [line[:-1].split(" ") for line in file1]
     for line in lines_list:
-        id_list.append(line[0])
+        if line[0] not in id_list:
+            id_list.append(line[0])
 
-
-with open(r"C:\Users\Martin\Desktop\python_test\testing2.txt", mode="w+", encoding="utf-8") as file2:
+with open(r"C:\Users\Martin\Desktop\Taxonomy\new_taxa.txt", mode="w+", encoding="utf-8") as file2:
     for i, id in enumerate(id_list):
         if any([id in line and id != line[0] for line in lines_list]):
+            pass
+        else:
             file2.write(" ".join(lines_list[i]) + "\n")
             print(i)
 
