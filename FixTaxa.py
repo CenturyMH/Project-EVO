08ef198830e2df4fe3ecddac64690c96eb71e3d5
unknown <martinhaghverdian@gmail.com>
2021-01-03 23:44:37 -0800

    new approach to algorithm

--- FixTaxa.py
+++ FixTaxa.py
@@ -1,22 +1,7 @@
 import re
 
-id_list = []
-with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r", encoding="utf-8") as file1:
-    lines_list = [line[:-1].split(" ") for line in file1]
-    for line in lines_list:
-        if line[0] not in id_list:
-            id_list.append(line[0])
 
-with open(r"C:\Users\Martin\Desktop\python_test\testing2.txt", mode="w+", encoding="utf-8") as file2:
-    for i, id in enumerate(id_list):
-        # Problem is most likely right here. Conditional logic probably needs more complexity.
-        if any([id in line and id != line[0] for line in lines_list]):
-            pass
-        else:
-            file2.write(" ".join(lines_list[i]) + "\n")
-            print(i)
 
-print(id_list, "\n", lines_list, sep="")
 # Killer Whale: 35489682
 # Next goal: Make every taxonID unique in the file to the species (Get rid of subspecies)
 # 3865179 lines were counted by looping through taxa.txt, took about 8-9 minutes
--- Marathon.py
+++ Marathon.py
@@ -1,10 +1,11 @@
 import re
 
-
 with open(r"C:\Users\Martin\Desktop\python_test\testing1.txt", mode="r", encoding="utf-8") as file1:
     with open(r"C:\Users\Martin\Desktop\python_test\testing2.txt", mode="w+", encoding="utf-8") as file2:
         for line in file1:
             id = re.search(r"([^\s]+)", line).group()
+            file2.write(id + "\n")
 
-# FixTaxa was modified. Do more testing, and figure out why it doesn't work on taxa.txt
-
+# Goal: do algorithm without using lists. Try writing one line at a time.
+# One line per loop iteration.
+# id gets new definition every loop, go from there
